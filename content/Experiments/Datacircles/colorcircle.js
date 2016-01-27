f11949e8d11b4b4f314de89260c6de190e0378d8
Ivo de Kler <ivo.de.kler@gmail.com>
2016-01-27 13:52:24 +0100

    changed colorcircle demo to be random as well

--- content/Experiments/Datacircles/colorcircle.js
+++ content/Experiments/Datacircles/colorcircle.js
@@ -1,25 +1,49 @@
 function initColorCircle(){
-	var $exampleDiv = $('#colorCircle');
+	var $exampleDiv = $( '#colorCircle' ),
+			$codeElement = $( 'code#editableRing' ),
+			circleData = randomCircleData1( 5, 50, 75 ),
+			dataCircle;
 
-	window.simpleSmartCanvas = new SmartCanvas({}, $exampleDiv);
+	window.simpleSmartCanvas = new SmartCanvas( {}, $exampleDiv );
 	simpleSmartCanvas.camera.rotation = -Math.PI / 2;
 
-	var dataCircle = new DataCircle();
-	simpleSmartCanvas.add(dataCircle);
+	createEditableCircle();
+	$codeElement.html( JSON.stringify( circleData, null, '\t' ), true );
 
-	$('code#editableRing').html(JSON.stringify(DataCircle.prototype.defaultData, null, '\t'), true).on('change', function(e){
-		var object = getObjectFromString($(this).html()),
-				index = simpleSmartCanvas.contents.indexOf(dataCircle);
+	$codeElement.on('change', codeChange );
+
+	$exampleDiv.on( 'click', newRandomCircle );
+
+	return;
+
+	function createEditableCircle() {
+		dataCircle = new DataCircle( circleData );
+		
+		simpleSmartCanvas.add( dataCircle );
+	}
+
+	function codeChange(e){
+		var object = getObjectFromString( $codeElement.html() ),
+				index = simpleSmartCanvas.contents.indexOf( dataCircle );
 		if(object){
-			$(this).css({color: 'white'});
-			simpleSmartCanvas.contents.splice(index, 1);
-			dataCircle = new DataCircle(object);
-			simpleSmartCanvas.add(dataCircle);
+			$codeElement.css( { color: 'white' } );
+			simpleSmartCanvas.contents.splice( index, 1 );
+			circleData = object;
+
+			createEditableCircle();
 		}
 		else{
-			$(this).css({color: 'red'});
+			$codeElement.css( { color: 'red' } );
 		}
-	});
+	}
+
+	function newRandomCircle(){
+		circleData = randomCircleData1( 5, 50, 75 );
+		simpleSmartCanvas.contents.splice( simpleSmartCanvas.contents.indexOf( dataCircle ), 1 );
+		$codeElement.html( JSON.stringify( circleData, null, '\t' ), true );
+
+		createEditableCircle();
+	}
 }
 
 initColorCircle();
