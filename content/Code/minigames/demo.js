e0e99c2717ec967f035186e35133fe8fe64e25ba
Ivo de Kler <ivo.de.kler@gmail.com>
2016-07-22 00:17:31 +0200

    hangman begin

--- content/Code/minigames/demo.js
+++ content/Code/minigames/demo.js
@@ -1,3 +1,7 @@
 $.get('/javascripts/Code/minigames/comperator.js', function(data) {
     $('#comperator').append('<code>' + data + '</code>');
 });
+
+$.get('/javascripts/Code/minigames/hangman.js', function(data) {
+    $('#hangman-container code').textContent = data;
+});
