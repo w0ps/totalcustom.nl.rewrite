f40e8ec0ff9823d75d34390540a9e75214334b49
Ivo de Kler <ivo.de.kler@gmail.com>
2016-09-02 09:45:23 +0200

    gradient reduction working (albeit suboptimally)

--- content/Experiments/Gradients/adaptive-gradient-buckets.js
+++ content/Experiments/Gradients/adaptive-gradient-buckets.js
@@ -1,7 +1,10 @@
-// todo a way to solve hard cuts aka very high difference in value over short distances
-function getBuckets( gradient, scalars, diffThreshold, maxBuckets ) {
-  
-  var bucketCount = 0,
+// todo: max buckets 2 doesn't work
+// also, improve performance by replacing transitions during reduction instead of calculating all of them from scratch
+function getBuckets( gradient, scalars, diffThreshold, max ) {
+  var maxBuckets = Math.max( 1, max ),
+      workingGradient = gradient.slice().sort( function comparePoints( a, b ) {
+        return a.pos - b.pos;
+      } ),
   // first create a set of functions to give differences between points
       scalarDifferentiators = scalars.map( function getScalarDifferentiator( scalar ) {
         var name = scalar.name || scalar,
@@ -11,54 +14,100 @@ function getBuckets( gradient, scalars, diffThreshold, maxBuckets ) {
           return weight * Math.abs( point1[ name ] - point2[ name ] );
         }
       } ),
-  // now establish all transitions between differing points
-      transitions = gradient.reduce( function observeChangedScalars( memo, point2, index, points ) {
-        
-        // a transition is the interaction between two control points, so ignore the first point
-        if( index ) {
-          var point1 = points[ index - 1 ],
-              difference = scalarDifferentiators.reduce( function getScalarDelta( total, scalarDifferentiator ) {
-            
-                return total + scalarDifferentiator( point1, point2 );  
-              }, 0 );
+      transitions, pointsByDiff, point1, point2, relativeD1;
+  do {
+    // now establish all transitions between differing points
+    transitions = workingGradient.reduce( function observeChangedScalars( memo, point2, index, points ) {
+      
+      // a transition is the interaction between two control points, so ignore the first point
+      if( index ) {
+        var point1 = points[ index - 1 ],
+            difference = scalarDifferentiators.reduce( function getScalarDelta( total, scalarDifferentiator ) {
+          
+              return total + scalarDifferentiator( point1, point2 );  
+            }, 0 );
 
-          if( difference ) {
-            memo.transitions.push( {
-              start: memo.startPoint,
-              end: point2,
-              length: point2.pos - memo.startPoint.pos,
-              diff: difference,
-              biasedLength: ( point2.pos - memo.startPoint.pos ) * difference,
-              steps: 1,
-              index: index
-            } );
+        if( difference ) {
+          memo.transitions.push( {
+            start: memo.startPoint,
+            end: point2,
+            length: point2.pos - memo.startPoint.pos,
+            diff: difference,
+            biasedLength: ( point2.pos - memo.startPoint.pos ) * difference,
+            steps: 1,
+            index: index
+          } );
 
-            memo.startPoint = point2;
-            ++bucketCount;
-          }
+          memo.startPoint = point2;
         }
+      }
 
-        return memo;
-      }, { transitions: [], startPoint: gradient[ 0 ] } ).transitions;
+      return memo;
+    }, { transitions: [], startPoint: workingGradient[ 0 ] } ).transitions;
 
-  // we need to cull the transitions in order of least difference
-  var transitionsByDiff = transitions.slice( 1, transitions.length - 1 ).sort( function compareTransitionsByDiff( a, b ) {
-        return b.diff - a.diff;
-      } ),
-      cullingEnabled = true;
+    // we need to cull the transitions in order of least difference
+    pointsByDiff = ( workingGradient.length >= 3 ? workingGradient : [] ).slice( 1, workingGradient.length - 1 ).map( function createPointDiff( point, index, points ) {
+      var transition1 = transitions[ index ],
+          transition2 = transitions[ index + 1 ];
 
-  while( transitions.length > maxBuckets || cullingEnabled ) {
+      return {
+        diff: transition1.diff + transition2.diff,
+        point: point
+      };
+    } ).sort( function comparepointsByDiff( a, b ) {
+      return b.diff - a.diff;
+    } );
+
     if (
-      transitions.length > maxBuckets
-      || ( cullingEnabled = transitionsByDiff.length && transitionsByDiff[ transitionsByDiff.length - 1 ].diff < diffThreshold )
+      workingGradient.length > maxBuckets
+      || ( pointsByDiff.length && pointsByDiff[ pointsByDiff.length - 1 ].diff < diffThreshold )
     ) {
-      transitions.splice( transitions.indexOf( transitionsByDiff.pop() ), 1 );
-      --bucketCount;
+      if( !pointsByDiff.length ) {
+        point1 = workingGradient[ 0 ];
+        point2 = workingGradient[ 1 ];
+        relativeD1 = ( point1.pos || !point2.pos ) ? point1.pos / ( point1.pos + ( 1 - point2.pos ) ) : 0.5;
+        
+        if( maxBuckets === 2 ) {
+          return [{
+            start: 0,
+            end: relativeD1,
+            t: point1.pos,
+            values: scalars.reduce( function getValues( memo, scalar ) {
+              return memo[ scalar.name || scalar ] = point1[ scalar.name || scalar ], memo;
+            }, {} )
+          }, {
+            start: relativeD1,
+            end: 1,
+            t: point2.pos,
+            values: scalars.reduce( function getValues( memo, scalar ) {
+              return memo[ scalar.name || scalar ] = point2[ scalar.name || scalar ], memo;
+            }, {} )
+          }];
+        } else {
+          return [ {
+            start: 0,
+            end: 1,
+            t: ( workingGradient[ 0 ].pos + workingGradient[ 1 ] ) / 2,
+            values: scalars.reduce( function getValues( memo, scalar ) {
+              return memo[ scalar.name || scalar ] = ipl(
+                point1[ scalar.name || scalar ],
+                point2[ scalar.name || scalar ],
+                relativeD1
+              ), memo;
+            }, {} )
+          } ];
+        }
+      }
+      workingGradient.splice( workingGradient.indexOf( pointsByDiff.pop().point ), 1 );
     }
-  }
+
+  } while ( workingGradient.length > maxBuckets );
 
   // adding detail to the transitions is done in order of diff * length
-  var transition;
+  var bucketCount = workingGradient.length,
+      transition,
+      transitionsByDiff;
+
   while( bucketCount < maxBuckets ) {
     transitionsByDiff = transitions.slice().sort( function( a, b ) {
       return b.biasedLength - a.biasedLength;
