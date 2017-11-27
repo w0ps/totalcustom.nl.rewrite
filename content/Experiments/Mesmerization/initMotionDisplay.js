fd5050f2dfb37a392425eb27a376d96b717363e6
Ivo de Kler <ivo.de.kler@gmail.com>
2017-11-27 13:30:36 +0100

    mesmerization demo

--- content/Experiments/Mesmerization/initMotionDisplay.js
+++ content/Experiments/Mesmerization/initMotionDisplay.js
@@ -1,6 +1,34 @@
-$('#initDemo').on('click', init);
+function initMiniField(){
+	var $miniFieldContainer = $('#minifield').empty(),
+			pixelRatio = window.devicePixelRatio || 1,
+			width = $miniFieldContainer.innerWidth(),
+			height = $miniFieldContainer.innerHeight(),
+			dynamicGridOptions = {
+				width: width,
+				height: height,
+				timePassing: true,
+				timeStep: 0.01
+			};
 
-function init(){
+	var motionDisplay = new MotionDisplay({
+		debugField: false,
+		gridOptions: dynamicGridOptions,
+		width: $miniFieldContainer.innerWidth(),
+		height: $miniFieldContainer.innerHeight(),
+	});
+
+	$miniFieldContainer.append(motionDisplay.canvas);
+
+	motionDisplay.canvas.style.width = width + 'px';
+	motionDisplay.canvas.style.height = height + 'px';
+}
+
+initMiniField();
+
+
+$('#initDemo').on('click', initFullscreen);
+
+function initFullscreen(){
 	var $overlay = $('body').overlay().css({
 		overflow: 'scroll'
 	});
@@ -16,65 +44,37 @@ function init(){
 				timeStep: 0.1
 			};
 
-	// var winAspect = $overlay.innerWidth / $overlay.innerHeight(),
-	// 	fieldAspect = importedGridOptions.width / importedGridOptions.height,
-		// 	wider = fieldAspect > winAspect,
-		// 	scale = wider ? $overlay.innerWidth() / importedGridOptions.width : $overlay.innerHeight() / importedGridOptions.height;
+	var motionDisplay = new MotionDisplay({
+		debugField: false,
+		gridOptions: dynamicGridOptions,
+		width: $overlay.innerWidth(),
+		height: $overlay.innerHeight(),
+	});
 
-		var motionDisplay = new MotionDisplay({
-			debugField: false,
-			gridOptions: dynamicGridOptions,
-			width: $overlay.innerWidth(),
-			height: $overlay.innerHeight(),
-		});
+	window.md = motionDisplay;
 
-		window.md = motionDisplay;
+	$overlay.append(motionDisplay.canvas);
 
-		$overlay.append(motionDisplay.canvas);
+	// motionDisplay.canvas.style.width = Math.floor(importedGridOptions.width * scale) + 'px';
+	// motionDisplay.canvas.style.height = Math.floor(importedGridOptions.height * scale) + 'px';
 
-		// motionDisplay.canvas.style.width = Math.floor(importedGridOptions.width * scale) + 'px';
-		// motionDisplay.canvas.style.height = Math.floor(importedGridOptions.height * scale) + 'px';
+	motionDisplay.canvas.style.width = width + 'px';
+	motionDisplay.canvas.style.height = height + 'px';
 
-		motionDisplay.canvas.style.width = width + 'px';
-		motionDisplay.canvas.style.height = height + 'px';
-
-		document.addEventListener('keyup', function(e){
-			console.log(e.keyCode);
-			if(e.keyCode === 70){
-				motionDisplay.debugField = !motionDisplay.debugField;
-				return;
-				if(motionDisplay.debugField){
-					motionDisplay.debugField = false;
-					motionDisplay.start();
-				} else {
-					motionDisplay.debugField = true;
-					//motionDisplay.running = false;
-				}
-
-				return;
+	document.addEventListener('keyup', function(e){
+		console.log(e.keyCode);
+		if(e.keyCode === 70){
+			motionDisplay.debugField = !motionDisplay.debugField;
+			return;
+			if(motionDisplay.debugField){
+				motionDisplay.debugField = false;
+				motionDisplay.start();
+			} else {
+				motionDisplay.debugField = true;
+				//motionDisplay.running = false;
 			}
 
-			if(e.keyCode === 86){
-				motionDisplay.grid.nextVariant();
-				return;
-			}
-
-			if(e.keyCode === 84){
-				motionDisplay.grid.nextTime();
-				return;
-			}
-
-			if(e.keyCode === 80){
-				motionDisplay.grid.toggleTimePassing();
-			}
-		});
-
-		$(motionDisplay.canvas).on('click', function(e){
-			console.log(motionDisplay.grid.getLocalV(
-				e.offsetX / (motionDisplay.width / motionDisplay.grid.width),
-				e.offsetY / (motionDisplay.height / motionDisplay.grid.height)
-			));
-		});
-
-	//});
+			return;
+		}
+	});
 }
